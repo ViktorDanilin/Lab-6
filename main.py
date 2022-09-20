889df3a2740dae735ef359e97d4674bccf1a9a75
ViktorDanilin <danilinvs58@gmail.com>
2022-09-20 14:46:51 +0300

    done

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class assault_rifle(guns):  # AR - M4; info from wikipedia.org
         self.__bullets += bullets
         super().__init__(self.__bullets, self.__f_rate, self.__f_range)
         self.time = super().calculation_1()
-        print("Новое время опустошения магазина пистолета: ", self.time)
+        print("Новое время опустошения магазина штурмовой винтовки: ", self.time)
         print("_______________________________________________________")
 
     def print(self):
@@ -72,7 +72,7 @@ class sniper_rifle(guns):  # SR - Винтовка Мосина; info from wikip
         self.__bullets += bullets
         super().__init__(self.__bullets, self.__f_rate, self.__f_range)
         self.time = super().calculation_1()
-        print("Новое время опустошения магазина пистолета: ", self.time)
+        print("Новое время опустошения магазина снайперской винтовки: ", self.time)
         print("_______________________________________________________")
 
     def print(self):
@@ -82,12 +82,39 @@ class sniper_rifle(guns):  # SR - Винтовка Мосина; info from wikip
         print("_______________________________________________________")
 
 
+class machinepistole(guns):  # MP - MP5; info from wikipedia.org
+    def __init__(self, bullets=30, f_rate=900, f_range=200):
+        self.__bullets = bullets
+        self.__f_rate = f_rate
+        self.__f_range = f_range
+        super().__init__(self.__bullets, self.__f_rate, self.__f_range)
+        self.time = super().calculation_1()
+        self.difference = super().calculation_2()
+
+    def reload(self, bullets): # увеличиваем кол-во патронов в магазине
+        self.__bullets += bullets
+        super().__init__(self.__bullets, self.__f_rate, self.__f_range)
+        self.time = super().calculation_1()
+        print("Новое время опустошения магазина пп: ", self.time)
+        print("_______________________________________________________")
+
+    def print(self):
+        print("Снайперская винтовка")
+        print("Магазин опустеет за", self.time, "секунды")
+        print("Соотношение скорострельности к дальности стрельбы: ", self.difference)
+        print("_______________________________________________________")
+
+
+
 pm = pistol()
 m4 = assault_rifle()
 sm = sniper_rifle()
+mp5 = machinepistole()
 pm.print()
 pm.reload(19)
 m4.print()
 m4.reload(60)
 sm.print()
 sm.reload(10)
+mp5.print()
+mp5.reload(60)
